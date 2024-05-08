529ca2a2761d98f91a0001e1cf2725003696c338
vmarquezv <vmarquezv@debian>
2024-05-08 11:38:20 -0300

    refact: config and mqtt requests

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import paho.mqtt.client as mqtt
 import requests
 import json
+import re
 
 from model.client import User, SimpleUser, LoginData, LoginSucess
 
-topic = '/topic/user/login'
+topic_pattern_user = re.compile('/esp8266.*/sessionid.*')
 url = 'http://0.0.0.0:5000'
 
 
 def on_connect(client, userdata, flags, reason_code):
     print(f"Connected with result code {reason_code}")
-
-    client.subscribe(topic, 2)
-
-
+    
 def req_login():
     r = requests.post(url+'/login', json={'name': 'Lua', 'password': '001'})
     ret = r.json()
@@ -22,9 +20,9 @@ def req_login():
     
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-   # req = msg.payload.json()
-   # print(req)
-    req_login()
+ #   req = msg.payload.json()
+ #   print(req)
+ #   req_login()
     
 def on_publish(client, userdata, result):
     print("data published \n")
@@ -35,8 +33,9 @@ mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 mqttc.on_publish = on_publish
 
-mqttc.connect("0.0.0.0", 8850, 60)
+mqttc.connect("192.168.12.1", 1883, 60)
 
-ret = mqttc.publish(topic, "{'user': 'Lua', 'pass': '001'}")
+mqttc.subscribe("#");
+
 
 mqttc.loop_forever()
